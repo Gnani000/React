ca519b7520438f6d81a46c29c9e2806ce82a42ab
viswaFED <narasingaviswanath@gmail.com>
2023-01-31 18:38:01 +0530

    First

---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container m-5">
+          <div className="row">
+            <div className="col-12">
+              <h3>About Us</h3>
+              <img src="https://www.lactame.com/github/image-js/image-js/3073b80c7d626196cb669f9d617f491a8338ca66/test/img/taxi/original.jpeg" />
+            </div>
+          </div>
+        </div>
+
+      </React.Fragment>
+    )
+  }
+}
